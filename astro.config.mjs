8e82becd7bb039dfa657932cbcc38338c60f8329
Filipp Dorofeev <f.dorofeev@alpinadigital.ru>
2024-09-21 22:03:17 +0200

    v.1.1.3
    static versel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: vercelServerless(),
+  output: 'static',
+  adapter: vercel(),
 });
