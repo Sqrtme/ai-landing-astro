2a1371eeb608a277d111aa30f71c50ed781b0316
Filipp Dorofeev <f.dorofeev@alpinadigital.ru>
2024-09-21 21:44:39 +0200

    v.1.1.2
    versel deploy pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import vercelServerless from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: vercelServerless(),
 });
